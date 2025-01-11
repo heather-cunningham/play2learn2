993d49e8acaa7b75ea880a0b4f6eb82e6b0cca64
heather-irene <cunningham.heatherirene@gmail.com>
2025-01-11 11:31:49 -0500

    anagram-hunt: Some BS style and layout for WordLengthInput, trying to get it to look like the rest of my Node website

--- public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/WordLengthInput.vue
+++ public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/WordLengthInput.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="anagram-hunt-select-div" class="game-select-div mx-auto mb-md-3
+  <div id="anagram-hunt-select-div" class="game-select-div w-75 mx-auto mb-md-3
          mb-sm-2 mb-2 d-flex justify-content-center align-items-center flex-wrap">
 
     <label id="word-len-lbl"
-           class="me-2 mb-2 fw-bold"
+           class="me-xl-3 me-lg-2 me-md-2 mb-2 fw-bold text-end"
            for="word-len-input">
       Word Length
     </label>
@@ -11,7 +11,7 @@
            class="mb-2 ps-2 pe-0 py-1"
            type="number"
            alt="Enter a number between 5 through 8"
-           placeholder="Please, enter a number from 5-8"
+           placeholder="Enter a # from 5-8"
            title="Enter a number between 5 through 8" />
   </div>
 </template>
@@ -31,11 +31,18 @@ export default defineComponent({
 }
 
 #word-len-input {
-  width: 50%;
+  width: 40%;
   height: 2.5rem;
   border: 1px solid gray;
   border-radius: 0.3rem;
   box-shadow: 0.17rem 0.17rem 0.17rem grey;
+}
+
+/* Custom styles for Bootstrap medium screens (768px wide) and smaller */
+@media (max-width: 768px) {
+  #anagram-hunt-select-div {
+    flex-direction: column;
+  }
 }
 
 </style>
