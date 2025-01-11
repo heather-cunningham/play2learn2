65267154352ffb458f07f2e93aa07190da6861ed
heather-irene <cunningham.heatherirene@gmail.com>
2025-01-11 11:41:06 -0500

    anagram-hunt: Some BS style and layout for GameRules.vue, trying to get it to look like the rest of my Node website

--- public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/GameRules.vue
+++ public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/GameRules.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="game-rules-list-div mx-auto mb-4 d-flex justify-content-center align-items-center flex-wrap">
+  <div class="game-rules-list-div mx-auto mb-4 d-flex justify-content-center align-items-center
+   flex-wrap">
     <ol class="game-rules-list">
       <li>Choose a word length between 5 to 8 characters long.</li>
       <li>Press "<strong>Play!</strong>"</li>
@@ -15,6 +16,21 @@ export default defineComponent({
   name: 'GameRules'
 });
 </script>
+
+<style scoped>
+.game-rules-list-div {
+  width: 50%;
+  height: auto;
+}
+
+/* Custom styles for Bootstrap medium screens (768px wide) and smaller */
+@media (max-width: 768px) {
+  .game-rules-list-div {
+    width: 75%;
+    height: auto;
+  }
+}
+</style>
 
 <style src="../../public/anagram-styles/anagram-normalize.css"></style>
 <style src="../../public/anagram-styles/anagram-style.css"></style>
