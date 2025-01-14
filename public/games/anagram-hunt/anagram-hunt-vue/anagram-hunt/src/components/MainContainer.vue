47983974092b967b992bb1fd480e397e8a3be434
heather-irene <cunningham.heatherirene@gmail.com>
2025-01-14 17:42:15 -0500

    anagram-hunt: Style for the question and answer input w/ Enter btn section. Removed styles for various #math-facts ids

--- public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/MainContainer.vue
+++ public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/MainContainer.vue
@@ -27,7 +27,6 @@
                      readonly />
             </div>
           </div>
-
           <div class="time-left-div row">
             <div id="time-lbl" class="col-md-4 col-sm-4 col-12 p-0 text-sm-end
                         text-start">
@@ -47,22 +46,39 @@
           </div>
         </div>
 
-        <div id="anagram-question-answer-div" class="question-answer-div text-center">
-          <label id="anagram-question-lbl"
-                 for="anagram-hunt-answer-input"
-                 class="question-lbl">
-            beard
-          </label>
-          <input id="anagram-hunt-answer-input"
-                 class="answer-input enabled"
-                 type="text"
-                 placeholder="Enter anagram here"
-                 value=""/>
+        <div id="anagram-question-answer-div"
+             class="question-answer-div row mx-md-auto mx-sm-auto mx-0 mb-4
+                    text-lg-center text-md-center text-sm-center text-center">
+          <div id="question-div" class="col-12 p-0">
+            <label id="anagram-question-lbl"
+                   for="anagram-hunt-answer-input"
+                   class="question-lbl mb-1">
+              beard
+            </label>
+          </div>
+          <div id="answer-div" class="col-12 mb-4 p-0">
+            <input id="anagram-hunt-answer-input"
+                   class="answer-input p-2 enabled"
+                   type="text"
+                   placeholder="Enter anagram here"
+                   value=""/>
+          </div>
+          <div id="enter-div" class="col-12 p-0 mt-1 mb-2">
+            <button id="anagram-enter-btn"
+                    class=""
+                    type="button"
+                    alt="Enter your anagram"
+                    title="Anagram Enter button"
+                    name="enter"
+                    value="Enter">
+              Enter
+            </button>
+          </div>
         </div>
 
-        <div id="anagram-user-answer-list-div">
+        <div id="anagram-user-answer-list-div" class="text-center">
           <ol id="anagram-user-answer-list">
-            <li>bread</li>
+            <li>bader</li>
           </ol>
         </div>
 
@@ -104,7 +120,8 @@ export default defineComponent({
 
 <style>
 .score-form {
-  width: 19rem;
+  width: 20rem;
+  overflow: hidden;
 }
 
 .score-input-div,
@@ -129,16 +146,72 @@ export default defineComponent({
 
 #anagram-score-lbl,
 #anagram-time-left-lbl {
-  margin: 0 0.167rem 0.167rem 0;
+  margin: 0 0.167rem 0.167rem 0.67rem;
   padding: 0 !important;
 }
 
 #anagram-score-input,
 #anagram-time-input {
   width: 13rem;
-  margin: 0 0 0.167rem 0.167rem;
+  margin: 0 0 0.167rem 0.67rem;
   padding: 0.167rem !important;
 }
+
+#anagram-question-answer-div {
+  width: 20rem !important;
+}
+
+.answer-input {
+  width: 20rem;
+  border: 1px solid grey;
+  box-shadow: 0.17rem 0.17rem 0.17rem grey;
+}
+
+#question-div,
+#answer-div,
+#enter-div,
+#anagram-enter-btn {
+  width: 20rem;
+}
+
+#anagram-enter-btn {
+  height: 2.5rem;
+  color: white;
+  background-color: green;
+  font-weight: bold;
+  font-size: larger;
+  border: none;
+  border-radius: 0.3rem;
+  box-shadow: 0.17rem 0.17rem 0.17rem grey;
+}
+
+.quit-btn:hover,
+#anagram-enter-btn:hover {
+  background-color: gray;
+}
+
+.quit-btn:active,
+#anagram-enter-btn:active {
+  background-color: gray;
+  border: 2px inset white;
+}
+
+/* Custom styles for Bootstrap small screens (576px wide) and smaller */
+@media (max-width: 576px) {
+  #anagram-score-lbl,
+  #anagram-time-left-lbl,
+  #anagram-score-input,
+  #anagram-time-input,
+  .answer-input,
+  #question-div,
+  #answer-div,
+  #enter-div,
+  #anagram-enter-btn {
+    width: 16rem;
+    text-align: center;
+  }
+}
+/* END Custom styles for Bootstrap small screens (576px wide) and smaller */
 </style>
 
 <style src="../../public/anagram-styles/anagram-normalize.css"></style>
