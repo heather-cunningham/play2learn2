b25c5b67344a1e59dda34fb087c366aa802e917b
heather-irene <cunningham.heatherirene@gmail.com>
2025-01-21 18:10:48 -0500

    anagram-hunt: put html template and fcns for GameQuestionAndAnswer back into GameBoard.vue.  I don't understand how to split up these comps, and class did not teach it

--- public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/GameBoard.vue
+++ public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/GameBoard.vue
@@ -12,8 +12,26 @@
       <p>The word length chosen is: {{ wordLength }}</p>
     </div>
 
-    <GameQuestionAndAnswer :word-length="wordLength" />
+    <div id="question-div" class="col-12 p-0">
+      <label id="anagram-question-lbl"
+             for="anagram-hunt-answer-input"
+             class="question-lbl mb-1">
+        {{ newAnagramWord }}
+      </label>
+    </div>
 
+    <div id="answer-div" class="col-12 mb-4 p-0">
+      <input id="anagram-hunt-answer-input"
+             class="answer-input p-2 enabled"
+             type="text"
+             placeholder="Enter anagram here"
+             alt="Type anagram here"
+             v-model="userAnswer"
+             @change="setAnswerInput" />
+    </div>
+
+    <EnterButton @click="handleClickEnterBtn"/>
+    <UserAnswers :users-answer-list="usersAnswerList"/>
   </div>
 </template>
 
@@ -21,7 +39,10 @@
 import {defineComponent} from "vue";
 import GameScore from "@/components/GameScore.vue";
 import GameTimer from "@/components/GameTimer.vue";
-import GameQuestionAndAnswer from "@/components/GameQuestionAndAnswer.vue";
+import EnterButton from "@/components/EnterButton.vue";
+import UserAnswers from "@/components/UserAnswers.vue";
+import {ANAGRAMS_LIST, removeElFromArray} from "@/assets/anagramsListHelpers";
+
 
 export default defineComponent({
   name: 'GameBoard',
@@ -37,15 +58,78 @@ export default defineComponent({
   components: {
     GameScore,
     GameTimer,
-    GameQuestionAndAnswer,
+    EnterButton,
+    UserAnswers,
   },
 
   data: ()=>{
     return {
       userScore: 0,
       timeLeft: 60,
+      newAnagramWordList: [],
+      newAnagramWord: "",
+      userAnswer: "",
+      isAnswerCorrect: false,
+      usersAnswerList: [],
     }
   },
+
+  created() {
+    this.setNewAnagram();
+  },
+
+  methods: {
+    setNewAnagram() {
+      // Word length between 5 and 8 chosen by user
+      const wordLenIndex = Number(this.wordLength);
+      const listOfWordLists = ANAGRAMS_LIST[wordLenIndex];
+
+      this.newAnagramWordList = listOfWordLists[Math.floor(Math.random() * listOfWordLists.length)];
+      console.log("####1 new anagram wordList1: " + this.newAnagramWordList);
+
+      const newAnagramIndex = Math.floor(Math.random() * this.newAnagramWordList.length);
+      this.newAnagramWord = this.newAnagramWordList[newAnagramIndex];
+      console.log("#### new anagram word: " + this.newAnagramWord);
+
+      // Remove the word from which the user is to make anagrams from the wordList
+      if (this.newAnagramWordList.includes(this.newAnagramWord))
+        this.newAnagramWordList = removeElFromArray(this.newAnagramWordList, newAnagramIndex);
+
+      console.log("####2 new anagram wordList2: " + this.newAnagramWordList);
+    },
+
+    resetAnagram() {
+      this.setNewAnagram();
+      this.usersAnswerList = [];
+      this.isAnswerCorrect = false;
+    },
+
+    setAnswerInput(){
+      const wordEntered = this.userAnswer.toLowerCase();
+      this.isAnswerCorrect = this.checkAnswer(wordEntered);
+
+      if(this.isAnswerCorrect){
+        this.newAnagramWordList = removeElFromArray(this.newAnagramWordList,
+            this.newAnagramWordList.indexOf(wordEntered));
+        this.usersAnswerList.push(wordEntered);
+        this.userAnswer = "";
+
+        // If all the anagrams have been guessed:
+        if(this.newAnagramWordList.length === 0)
+          this.resetAnagram();
+      }
+    },// end setAnswerInput()
+
+    handleClickEnterBtn() {
+      this.setAnswerInput();
+    },
+
+    checkAnswer(wordEntered){
+      if(this.newAnagramWordList.includes(wordEntered)){
+        return true;
+      }
+    },
+  },//end methods
 });
 </script>
 
@@ -55,9 +139,28 @@ export default defineComponent({
   overflow: hidden;
 }
 
-#anagram-question-answer-div {
+#anagram-question-answer-div,
+#question-div,
+#answer-div,
+.answer-input {
   width: 20rem !important;
 }
+
+.answer-input {
+  border: 1px solid grey;
+  box-shadow: 0.17rem 0.17rem 0.17rem grey;
+}
+
+/* Custom styles for Bootstrap small screens (576px wide) and smaller */
+@media (max-width: 576px) {
+  #anagram-question-answer-div,
+  #question-div,
+  #answer-div,
+  .answer-input {
+    width: 16rem;
+    text-align: center;
+  }
+} /* END Custom styles for Bootstrap small screens (576px wide) and smaller */
 </style>
 
 <style src="../../public/anagram-styles/anagram-normalize.css"></style>
