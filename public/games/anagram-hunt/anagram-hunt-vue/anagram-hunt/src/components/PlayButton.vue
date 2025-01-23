bea27b1681b740a37fe56698daec069b3edf61a3
heather-irene <cunningham.heatherirene@gmail.com>
2025-01-23 15:14:19 -0500

    anagram-hunt: BS width class for outer div

--- public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/PlayButton.vue
+++ public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/PlayButton.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="site-page-div mx-auto mb-md-4 mb-sm-3 mb-3 my-0 d-flex justify-content-center align-items-center
-              flex-wrap">
+  <div id="anagram-play-btn-div"
+       class="site-page-div w-75 mx-auto mb-md-4 mb-sm-3 mb-3 my-0 d-flex justify-content-center
+              align-items-center flex-wrap">
     <button id="anagram-play-btn"
             class="btn"
             type="button"
