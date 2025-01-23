ca4a1e3447de2f2a0e2ffb131fb197e15a1ab878
heather-irene <cunningham.heatherirene@gmail.com>
2025-01-23 15:15:52 -0500

    anagram-hunt: Citation w/ Note about Anagram rules

--- public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/GameRules.vue
+++ public/games/anagram-hunt/anagram-hunt-vue/anagram-hunt/src/components/GameRules.vue
@@ -2,10 +2,14 @@
   <div class="game-rules-list-div mx-auto my-2 d-flex justify-content-center align-items-center
    flex-wrap">
     <ol class="game-rules-list">
-      <li>Choose a word length between 5 to 8 characters long.</li>
+      <li>Choose a word length between 5 and 8 characters long.</li>
       <li>Press "<strong>Play!</strong>"</li>
       <li>How many anagrams can you find in a minute?</li>
     </ol>
+    <cite class="mb-2">
+      <span class="fw-bold">Note:</span>
+      All letters of the word must be used in the anagram to count as a correct answer.
+    </cite>
   </div>
 </template>
 
